53dbb03d16a0c51ec07db20045a8c80ff23d52ab
kanbilly <b.kan@lancaster.ac.uk>
2017-03-20 21:25:16 +0000

    Add score label and buttons for  Task 2

--- src/GameDriver.java
+++ src/GameDriver.java
@@ -1,26 +1,114 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  * Task 1: Create a Working Demonstrator. <br\>
+ * Task 2: Adding a Score and two buttons. <br\>
  * This is my own version main class that uses the software provided to play the
  * basic game in a swing window
  *
  * @author kan billy (b.kan@lancaster.ac.uk)
  *
  */
-public class GameDriver {
+public class GameDriver implements ActionListener {
+	private JLabel scoreLabel;
+	private JButton startButton, stopButton;
+	private Racer r;
 
-	public static void main(String args[]) {
+	/**
+	 * Default constructor, used to initialize all the components
+	 */
+	public GameDriver() {
 		JFrame window = new JFrame();
-		Racer r = new Racer();
+		BoxLayout boxLayout = new BoxLayout(window.getContentPane(),
+				BoxLayout.Y_AXIS);
+		window.setLayout(boxLayout);
+
+		r = new Racer();
 		window.setTitle("Racer");
 		window.setSize(Racer.SCREEN_WIDTH, Racer.SCREEN_HEIGHT);
-		window.setContentPane(r.getPanel());
+		window.add(topPanel());
+		window.add(r.getPanel());
+		window.add(bottomPanel());
 		window.setVisible(true);
+	}
 
+	/**
+	 * The panel contains the score record of the player
+	 *
+	 * @return the top panel
+	 */
+	private JPanel topPanel() {
+		JPanel pane = new JPanel();
+		pane.setLayout(new BorderLayout());
+		scoreLabel = new JLabel("Score: ");
+		pane.add(scoreLabel, BorderLayout.WEST);
+		return pane;
+	}
+
+	/**
+	 * The panel contains start and stop buttons.
+	 *
+	 * @return The bottom panel
+	 */
+	private JPanel bottomPanel() {
+		JPanel pane = new JPanel();
+		pane.setLayout(new GridLayout(1, 2));
+
+		startButton = new JButton("PLAY");
+		startButton.addActionListener(this);
+		stopButton = new JButton("STOP");
+		stopButton.addActionListener(this);
+
+		pane.add(startButton);
+		pane.add(stopButton);
+		return pane;
+	}
+
+	/**
+	 * The main entrance of the program
+	 *
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		GameDriver game = new GameDriver();
+		game.play();
+	}
+
+	/**
+	 * The play function as a routine
+	 */
+	public void play() {
 		r.start();
+		while (true) {
+			// make the racer able to render the graphical lock
+			r.getArena().pause();
+			while (r.isPlaying()) {
+				r.update();
+				// update the real time score
+				scoreLabel.setText("Score:" + r.getScore());
+			}
+		}
+	}
 
-		while (r.isPlaying())
-			r.update();
+	/**
+	 * Action Listeners for the start and stop button
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object o = e.getSource();
+		if (o == startButton) {
+			r.start();
+		} else if (o == stopButton) {
+			r.stop();
+		}
 	}
 }
--- src/Racer.java
+++ src/Racer.java
@@ -1,227 +1,219 @@
-import javax.swing.*;
+import javax.swing.JComponent;
+
 /**
  * uses the GameArena APIs to implement a simple top down racing game.
  *
- * The graphical output of the game is provided as a Swing component,
- * so that it can be added into any Swing application, just like a JButton etc.
+ * The graphical output of the game is provided as a Swing component, so that it
+ * can be added into any Swing application, just like a JButton etc.
  *
- * To allow users to control the game as they see fit, start(), stop() and update()
- * methods are provided. start() should be used to create a new game, stop() to terminate
- * a running game, and update() should be called in a loop to update gameplay and graphics
+ * To allow users to control the game as they see fit, start(), stop() and
+ * update() methods are provided. start() should be used to create a new game,
+ * stop() to terminate a running game, and update() should be called in a loop
+ * to update game play and graphics
  *
  * Simple example of use:
  *
  * <pre>
  *
- *  JFrame window = new JFrame();
- *  Racer r = new Racer();
- *  window.setTitle("Racer");
- *  window.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
- *  window.setContentPane(r.getPanel());
- *  window.setVisible(true);
- *
- *  r.start();
- *
- *  while(r.isPlaying())
- *      r.update();
+ * JFrame window = new JFrame();
+ * Racer r = new Racer();
+ * window.setTitle(&quot;Racer&quot;);
+ * window.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+ * window.setContentPane(r.getPanel());
+ * window.setVisible(true);
+ * 
+ * r.start();
+ * 
+ * while (r.isPlaying())
+ * 	r.update();
  *
  * </pre>
  *
  * @author Joe Finney (joe@comp.lancs.ac.uk)
  */
-public class Racer 
-{
-    public static final double PLAYER_SPEED = 5;
-    public static final int ROAD_SEGMENT_WIDTH = 160;
-    public static final int ROAD_SEGMENT_HEIGHT= 10;
-    public static final int ROAD_CURVE_SPEED = 5;
-    public static final int SCREEN_WIDTH = 800;
-    public static final int SCREEN_HEIGHT = 600;
+public class Racer {
+	public static final double PLAYER_SPEED = 5;
+	public static final int ROAD_SEGMENT_WIDTH = 160;
+	public static final int ROAD_SEGMENT_HEIGHT = 10;
+	public static final int ROAD_CURVE_SPEED = 5;
+	public static final int SCREEN_WIDTH = 800;
+	public static final int SCREEN_HEIGHT = 600;
 
-    private GameArena arena;
-    private Car player;
-    private RoadSegment[] road = new RoadSegment[SCREEN_HEIGHT / ROAD_SEGMENT_HEIGHT + 1];
+	private GameArena arena;
+	private Car player;
+	private RoadSegment[] road = new RoadSegment[SCREEN_HEIGHT
+			/ ROAD_SEGMENT_HEIGHT + 1];
 
-    private double currentRoadX = SCREEN_WIDTH/2;
-    private double speed = 2.0;
-    private boolean playing = false;
-    private int score = 0;
+	private double currentRoadX = SCREEN_WIDTH / 2;
+	private double speed = 2.0;
+	private boolean playing = false;
+	private int score = 0;
 
-    /**
-     * Creates a new instance of the Racer racing game.
-     */
-    public Racer()
-    {
-        arena = new GameArena(SCREEN_WIDTH, SCREEN_HEIGHT, false);
-    }
+	/**
+	 * Creates a new instance of the Racer racing game.
+	 */
+	public Racer() {
+		arena = new GameArena(SCREEN_WIDTH, SCREEN_HEIGHT, false);
+	}
 
-    /**
-     * Provides a Swing component in which the Racer game runs.
-     * This component can be added to a Swing panel to display the game on screen.
-     *
-     * @return A Swing component for this game.
-     */
-    public JComponent getPanel()
-    {
-        return arena.getPanel();
-    }
+	/**
+	 * Provides a Swing component in which the Racer game runs. This component
+	 * can be added to a Swing panel to display the game on screen.
+	 *
+	 * @return A Swing component for this game.
+	 */
+	public JComponent getPanel() {
+		return arena.getPanel();
+	}
 
-    /**
-     * Provides the player's current score in the game.
-     * @return The player's current score.
-     */
-    public int getScore()
-    {
-        return score;
-    }
+	/**
+	 * Provides the player's current score in the game.
+	 *
+	 * @return The player's current score.
+	 */
+	public int getScore() {
+		return score;
+	}
 
-    /**
-     * Starts a new game, if the game is not alreayd running.
-     */
-    public void start()
-    {
-        if(!playing)
-        {
-            // Create the player's car
-            player = new Car(SCREEN_WIDTH/2, SCREEN_HEIGHT - 150, arena);
+	/**
+	 * Provides the scene of the game
+	 *
+	 * @return The scene of the game
+	 */
+	public GameArena getArena() {
+		return arena;
+	}
 
-            // Create the initial road layout
-            for (int s = road.length-1; s >= 0 ; s--)
-            {
-                road[s] = nextRoadSegment(); 
-                road[s].setYPosition(s*ROAD_SEGMENT_HEIGHT);
-            }
+	/**
+	 * Starts a new game, if the game is not already running.
+	 */
+	public void start() {
+		if (!playing) {
+			// Create the player's car
+			player = new Car(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 150, arena);
 
-            score = 0;
-            playing = true;
-        }
-    }
+			// Create the initial road layout
+			for (int s = road.length - 1; s >= 0; s--) {
+				road[s] = nextRoadSegment();
+				road[s].setYPosition(s * ROAD_SEGMENT_HEIGHT);
+			}
 
-    /**
-     * Stops a currently running game.
-     */
-    public void stop()
-    {
-        if(playing)
-        {
-            playing = false;
-            arena.exit();
-        }
-    }
+			score = 0;
+			playing = true;
+		}
+	}
 
-    /**
-     * Determines if the game is currently being played.
-     *
-     * @return false if the game has not been started or on game over, true if the game is actively running.
-     */
-    public boolean isPlaying()
-    {
-        return playing;
-    }
+	/**
+	 * Stops a currently running game.
+	 */
+	public void stop() {
+		if (playing) {
+			playing = false;
+			arena.exit();
+		}
+	}
 
-    /**
-     * Updates the game state to allow the road and player character to move on the screen.
-     *
-     * This method should be called in a loop (once per frame) to advance gameplay in response to time
-     * and user input. The method uses the GameArena pause() method to ensure the game runs at a constant speed.
-     */
-    public void update()
-    {
-        if(playing)
-        {
-            score++;
+	/**
+	 * Determines if the game is currently being played.
+	 *
+	 * @return false if the game has not been started or on game over, true if
+	 *         the game is actively running.
+	 */
+	public boolean isPlaying() {
+		return playing;
+	}
 
-            double speed = 0;
-            if (arena.leftPressed())
-                speed -= PLAYER_SPEED;
+	/**
+	 * Updates the game state to allow the road and player character to move on
+	 * the screen.
+	 *
+	 * This method should be called in a loop (once per frame) to advance game
+	 * play in response to time and user input. The method uses the GameArena
+	 * pause() method to ensure the game runs at a constant speed.
+	 */
+	public void update() {
+		if (playing) {
+			score++;
 
-            if (arena.rightPressed())
-                speed += PLAYER_SPEED;
+			double xspeed = 0;
+			double yspeed = 0;
+			if (arena.leftPressed())
+				xspeed -= PLAYER_SPEED;
 
-            player.setXSpeed(speed);
+			if (arena.rightPressed())
+				xspeed += PLAYER_SPEED;
 
-            player.move();
-            for (int i=0; i<road.length; i++)
-            {
-                if (road[i] != null)
-                    road[i].move();
-            }
+			if (arena.upPressed())
+				yspeed -= PLAYER_SPEED;
 
-            // Recycle any segments that have crolled off screen...
-            recycleRoadSegments();
+			if (arena.downPressed())
+				yspeed += PLAYER_SPEED;
 
-            if (hasCrashed())
-                stop();
-        }
+			player.setXSpeed(xspeed);
+			player.setYSpeed(yspeed);
 
-        arena.pause();
-    }
+			player.move();
 
-    /**
-     * Provides a randomly generated, thin slice of road. 
-     * This method is used periodically to create new road on the screen in front of the player's car.
-     *
-     * @return A new randomly generated RoadSegment
-     */
-    private RoadSegment nextRoadSegment()
-    {
-        currentRoadX += Math.random() * 2 * ROAD_CURVE_SPEED - ROAD_CURVE_SPEED;
-        RoadSegment s = new RoadSegment(currentRoadX, -ROAD_SEGMENT_HEIGHT, ROAD_SEGMENT_WIDTH, ROAD_SEGMENT_HEIGHT, arena);
-        s.setYSpeed(speed);
-        return s;
-    }
+			for (int i = 0; i < road.length; i++) {
+				if (road[i] != null)
+					road[i].move();
+			}
 
-    /**
-     * Removes any parts of road that have scrolled off the bottom of the screen.
-     */
-    private void recycleRoadSegments()
-    {
-        for (int i=0; i<road.length; i++)
-        {
-            if (road[i].getYPosition() > SCREEN_HEIGHT)
-            {
-                double y = road[i].getYPosition();
-                road[i].remove();
-                road[i] = nextRoadSegment(); 
-                road[i].setYPosition(y - SCREEN_HEIGHT - ROAD_SEGMENT_HEIGHT);
-            }
-        }
-    }
+			// Recycle any segments that have scrolled off screen...
+			recycleRoadSegments();
 
-    /**
-     * Determines if the player has crased (driven off road)
-     *
-     * @return true is the player is touching the kerb/grass, false otherwise.
-     */
-    private boolean hasCrashed()
-    {
-        for (int i=0; i<road.length; i++)
-        {
-            if (player.isTouching(road[i]))
-                return true;
-        }
+			if (hasCrashed())
+				stop();
+		}
 
-        return false;
-    }
+		arena.pause();
+	}
 
-    /**
-     * A simple example of usage
-     *
-     * @param args unused.
-     */
-    public static void main(String[] args)
-    {
-        JFrame window = new JFrame();
-        Racer r = new Racer();
+	public void pause() {
+		arena.pause();
+	}
 
-        window.setTitle("Racer");
-        window.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-        window.setContentPane(r.getPanel());
-        window.setVisible(true);
+	/**
+	 * Provides a randomly generated, thin slice of road. This method is used
+	 * periodically to create new road on the screen in front of the player's
+	 * car.
+	 *
+	 * @return A new randomly generated RoadSegment
+	 */
+	private RoadSegment nextRoadSegment() {
+		currentRoadX += Math.random() * 2 * ROAD_CURVE_SPEED - ROAD_CURVE_SPEED;
+		RoadSegment s = new RoadSegment(currentRoadX, -ROAD_SEGMENT_HEIGHT,
+				ROAD_SEGMENT_WIDTH, ROAD_SEGMENT_HEIGHT, arena);
+		s.setYSpeed(speed);
+		return s;
+	}
 
-        r.start();
+	/**
+	 * Removes any parts of road that have scrolled off the bottom of the
+	 * screen.
+	 */
+	private void recycleRoadSegments() {
+		for (int i = 0; i < road.length; i++) {
+			if (road[i].getYPosition() > SCREEN_HEIGHT) {
+				double y = road[i].getYPosition();
+				road[i].remove();
+				road[i] = nextRoadSegment();
+				road[i].setYPosition(y - SCREEN_HEIGHT - ROAD_SEGMENT_HEIGHT);
+			}
+		}
+	}
 
-        while(r.isPlaying())
-            r.update();
-    }
+	/**
+	 * Determines if the player has crashed (driven off road)
+	 *
+	 * @return true is the player is touching the kerb/grass, false otherwise.
+	 */
+	private boolean hasCrashed() {
+		for (int i = 0; i < road.length; i++) {
+			if (player.isTouching(road[i]))
+				return true;
+		}
+
+		return false;
+	}
 }
